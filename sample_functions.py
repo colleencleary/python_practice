062e3b54c18e91e37fbdd3df828686ce79de393a
Colleen Cleary <cleary.ce@gmail.com>
2016-06-01 14:48:59 -0400

    addedlist of first set of functions

---
+++ sample_functions.py
@@ -0,0 +1,11 @@
+'''This will be a collection of our first set of functions'''
+
+# our first function
+def max_of_three(arg1, arg2, arg3):
+    '''This function takes in three functions and returns the largest value'''
+    if (arg1>arg2 and arg1>arg3):
+            return arg1
+    elif (arg2>arg3):
+            return arg2
+    else:
+            return arg3
