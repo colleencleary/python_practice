cf11dfe802676d57c24deaa2763206acbf02b772
Colleen Cleary <cleary.ce@gmail.com>
2016-06-02 13:42:51 -0400

    update

--- sample_graph.py
+++ sample_graph.py
@@ -2,7 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-mydata=np.loadtxt(data.txt)
+mydata=np.loadtxt('data.txt')
 x=mydata[:,0]
 y=mydata[:,1]
 plt.plot(x,y, color='green', marker='*')
