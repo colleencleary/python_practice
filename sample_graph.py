1be2c7af63d5984ce8bf9b83ab05c22d8b2e2e05
Colleen Cleary <cleary.ce@gmail.com>
2016-06-02 13:40:18 -0400

    update

--- sample_graph.py
+++ sample_graph.py
@@ -1,8 +1,9 @@
+'''This is a sample graph'''
 import numpy as np
-import matplotlib.plot as plt
-%matplotlib inline
+import matplotlib.pyplot as plt
 
+mydata=np.loadtxt(mydata.txt)
 x=mydata[:,0]
 y=mydata[:,1]
 plt.plot(x,y, color='green', marker='*')
-
+plt.show()
