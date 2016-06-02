37d4ddf3d2d72b8f901654c498b34854572d7215
Colleen Cleary <cleary.ce@gmail.com>
2016-06-02 10:02:56 -0400

    New file with sample classes

---
+++ sample_classes.py
@@ -0,0 +1,15 @@
+class simple_arithmetic:
+    '''This object takes two arguments at initialization. 
+    There are four methods that will return simple arithmetic operations.'''
+    def __init__(self, arg1, arg2):
+        self.arg1=arg1
+        self.arg2=arg2
+    def add(self):
+        return self.arg1+self.arg2
+    def sub(self):
+        return self.arg1-self.arg2
+    def div(self):
+        return float(self.arg1)/self.arg2
+    def mul(self):
+        return self.arg1*self.arg2
+        
